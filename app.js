173a33a4aba76e63f5cf5bf908de745a8edbfe5f
Hitesh Kumar <hitesh.k.83080@gmail.com>
2022-09-17 23:14:01 +0530

    Update Port

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 80 ;
 const bodyParser = require("body-parser");
 const https = require("https");
 
@@ -73,4 +72,6 @@ app.post("/", function (req, res) {
   });
 });
 
-app.listen(port);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
